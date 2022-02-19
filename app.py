486f3395757176707f503cab82eeea7232be61c7
Runtas, James <jamesruntas@cmail.carleton.ca>
2022-02-19 18:36:02 -0500

    api call

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 import requests
 
 
-APIKEY = "8fcad4b1-127e-4634-b97a-b20de7b0ed80"
-dictionary = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"
-suffix = "?key=8fcad4b1-127e-4634-b97a-b20de7b0ed80"
 
 
 
-def main():
-    word = "Apple"
-    query = word+suffix
 
-    response = requests.request("GET", dictionary, headers=query)
-    print(response.text)
+def query_dictionary(key, word):
+    query = requests.get('https://dictionaryapi.com/api/v3/references/collegiate/json/' + word + '?key=' + key)
+    response = query.json()
+    try:
+        returnDict = 1
+    except:
+        returnDict = null
+        
+    print (word)
+    print (response)
+    return response
     
 
+    
+if __name__=="__main__":
+    key = "8fcad4b1-127e-4634-b97a-b20de7b0ed80"
+    query_dictionary(key, "apple")
+    	
+    	
+    	
+    	
+    	
+    	
+    	
 
 
 if __name__=="__main__":
